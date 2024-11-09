e631aa54b6c09a2fb9ae0a856ae5b2ebaba1f703
myuR <myurpvn@gmail.com>
2024-11-09 16:42:49 +0530

    better calc logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-// #![allow(dead_code)]
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum Operation {
     Addition,
     Substraction,
@@ -14,92 +12,127 @@ impl Operation {
             Self::Addition => x + y,
             Self::Substraction => x - y,
             Self::Multiplication => x * y,
-            Self::Division => x / y,
+            Self::Division => {
+                if y == 0 {
+                    -1
+                } else {
+                    x / y
+                }
+            }
         }
     }
 }
+#[derive(Debug, PartialEq, Eq)]
+enum Input {
+    Number(i32),
+    Operation(Operation),
+    Unsupported,
+}
 
-fn do_calculations(temp: &mut i32, numbers: &Vec<i32>, ops: &Vec<Operation>) {
-    let mut nums_iter = numbers.iter();
-    let mut ops_iter = ops.iter();
+fn calculate_input(input_vec: &Vec<Input>) -> i32 {
+    let mut result_vec: Vec<i32> = Vec::new();
+    let mut ops_vec: Vec<&Operation> = Vec::new();
 
-    let num1 = nums_iter.next().unwrap_or(&0);
-    let num2 = nums_iter.next().unwrap_or(&0);
+    for input in input_vec {
+        match input {
+            Input::Number(num) => result_vec.push(*num),
+            Input::Operation(operation) => ops_vec.push(operation),
+            _ => {}
+        }
+        if result_vec.len() == 2 {
+            let num2 = result_vec.pop().unwrap();
+            let num1 = result_vec.pop().unwrap();
+            let ops = ops_vec.pop().unwrap();
 
-    if let Some(op) = ops_iter.next() {
-        *temp = op.calculate(*num1, *num2);
+            if ops == &Operation::Division && num2 == 0 {
+                println!("Zero Division Encountered");
+            }
+            let r = ops.calculate(num1, num2);
+            result_vec.push(r);
+        }
     }
+
+    result_vec.pop().unwrap()
 }
 
-fn alter_op_flag(op_flag: &mut bool) {
-    *op_flag = !*op_flag;
+fn split_input(s: &str) -> (bool, Vec<Input>) {
+    let mut splitted_input: Vec<Input> = Vec::new();
+    let mut num_string: String = String::new();
+    let mut is_valid = true;
+
+    let mut s_iter = s.chars(); // "55+5+6"
+    let mut expect_num = true;
+
+    while let Some(c) = s_iter.next() {
+        if c.is_numeric() {
+            num_string.push(c);
+        } else {
+            if num_string.len() > 0 {
+                if expect_num {
+                    splitted_input.push(Input::Number(num_string.parse().unwrap()));
+                    expect_num = false;
+                    num_string.clear();
+                } else {
+                    is_valid = false;
+                    break;
+                }
+            }
+            let input_ops: Input = match c {
+                '+' => Input::Operation(Operation::Addition),
+                '-' => Input::Operation(Operation::Substraction),
+                '*' => Input::Operation(Operation::Multiplication),
+                '/' => Input::Operation(Operation::Division),
+                _ => Input::Unsupported,
+            };
+
+            if !expect_num {
+                splitted_input.push(input_ops);
+                expect_num = true;
+            } else {
+                is_valid = false;
+                break;
+            }
+        }
+    }
+
+    if num_string.len() > 0 && expect_num {
+        splitted_input.push(Input::Number(num_string.parse().unwrap()));
+    } else {
+        is_valid = false;
+    }
+
+    (is_valid, splitted_input)
 }
+
 fn main() {
     use std::io::{stdin, stdout, Write};
+    println!("============ Welcome to the Rust Calculator ============");
 
-    println!("Welcome to the rust calculator");
+    loop {
+        print!("Enter Expression: ");
 
-    let mut numbers: Vec<i32> = Vec::new();
-    let mut ops: Vec<Operation> = Vec::new();
-    let mut result: i32 = 0;
-    let mut expect_op: bool = false;
-
-    let mut s = String::new();
-    let mut loop_flag = true;
-
-    while loop_flag {
-        if expect_op {
-            print!("Enter Operation: ");
-        } else {
-            print!("Enter Value: ");
-        }
-
+        let mut s: String = String::new();
         let _ = stdout().flush();
         stdin()
             .read_line(&mut s)
             .expect("Did not enter a correct string");
 
-        let byte_val = s.trim().as_bytes();
+        s = s.trim().to_string();
 
-        if expect_op {
-            match byte_val {
-                b"+" => ops.push(Operation::Addition),
-                b"-" => ops.push(Operation::Substraction),
-                b"*" => ops.push(Operation::Multiplication),
-                b"/" => ops.push(Operation::Division),
-                b"=" => {
-                    loop_flag = false;
-                    println!("Result: {result}");
-                }
-                _ => {
-                    println!("Expecting Operation!");
-                    println!("Please enter a valid Operation!");
-                    alter_op_flag(&mut expect_op);
-                }
-            }
+        if s.chars().next().unwrap_or('_') == 'q' {
+            break;
+        }
+
+        let (is_valid, splitted_input) = split_input(&s);
+
+        if is_valid {
+            let result = calculate_input(&splitted_input);
+            println!("Result: {result}\n");
         } else {
-            use std::str::from_utf8;
-
-            if let Ok(num) = from_utf8(byte_val).unwrap().parse::<i32>() {
-                numbers.push(num);
-            } else {
-                println!("Entered value '{}' is not a valid Number", &s.trim());
-                println!("Please enter a valid Number!");
-                alter_op_flag(&mut expect_op);
-            }
+            println!("Input is not valid!");
+            println!("Input: {s}");
         }
-
-        if numbers.len() == 2 {
-            do_calculations(&mut result, &numbers, &ops);
-            ops.clear();
-            numbers.clear();
-            numbers.push(result);
-        }
-
-        s.clear();
-        alter_op_flag(&mut expect_op);
-        // println!("Ops: {ops:?}, Nums: {numbers:?}");
     }
 
-    // println!("Ops: {ops:?}, Nums: {numbers:?}");
+    println!("Bye :)")
 }
